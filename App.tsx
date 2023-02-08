9a0833d479f9e71afb4e9388cf6f35b1d06600d7
Maxime Tchagou <maxime.tchagou.1@ulaval.ca>
2023-02-08 01:10:04 -0500

    feat: init feed view

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FeedView from './views/feed';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FeedView/>
       <StatusBar style="auto" />
     </View>
   );
